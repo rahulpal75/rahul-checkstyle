5ef68383cc0bc17d02130c0cc418bb065a0c99de
rahulpal75 <rahul.pal35@outlook.com>
2022-11-21 19:55:13 +0530

    Add checkstyle codes

---
+++ src/main/java/com/rahul/checks/LimitImplementationFiles.java
@@ -0,0 +1,40 @@
+package com.rahul.checks;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractFileSetCheck;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+
+import java.io.File;
+
+public class LimitImplementationFiles extends AbstractFileSetCheck
+{
+    private static final int DEFAULT_MAX = 100;
+    private int fileCount;
+    private int max = DEFAULT_MAX;
+    public void setMax(int aMax)
+    {
+        this.max = aMax;
+    }
+
+    @Override
+    protected void processFiltered(File file, FileText fileText) throws CheckstyleException {
+        this.fileCount++;
+
+        if (this.fileCount > this.max) {
+            // log the message
+            log(1, "max.files.exceeded", Integer.valueOf(this.max));
+            // you can call log() multiple times to flag multiple
+            // violations in the same file
+        }
+    }
+
+    @Override
+    public void beginProcessing(String aCharset)
+    {
+        super.beginProcessing(aCharset);
+
+        //reset the file count
+        this.fileCount = 0;
+    }
+
+}
